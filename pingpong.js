340256eb7a346811ea18eb732f6b8e12fa7ef22b
Zhenzhen You <swimreally@MacBook-Air.local>
2018-03-22 17:28:32 -0700

    add reduce method

--- pingpong.js
+++ pingpong.js
@@ -2,12 +2,19 @@ function Add(numStr) {
   if (numStr === '') {
     return 0;
   }
-  numArr = numStr.split(',');
-  var total = 0;
-  for (let num of numArr) {
-    total += parseFloat(num);
-  };
+  var numArr = numStr.split(/[,\n]/);
+
+  // var total = 0;
+  // for (let num of numArr) {
+  //   total += parseFloat(num);
+  // };
+  // return total;
+
+  var total = numArr.reduce((accu, curr) => {
+    return parseFloat(accu) + parseFloat(curr);
+  }, 0);
   return total;
+
 }
 
 
@@ -23,4 +30,14 @@ describe ("Add", function() {
   it("should return sum if two nums", function() {
     expect(Add("1,5")).toBe(6);
   });
+
+  it("should handle an unknown amount of numbers", function() {
+    expect(Add("1,2,3,4,5")).toBe(15);
+    expect(Add("1,5,6")).toBe(12);
+  });
+
+  it("should handle new lines between numbers", function() {
+    expect(Add("1\n2,3")).toBe(6);
+  });
+
 });
