3794bb50c2d292c80d1252d6a1a74020fee6c9a1
Zhenzhen You <swimreally@MacBook-Air.local>
2018-03-26 13:44:00 -0700

    add uniqueInOrder test

---
+++ uniqueInOrder.js
@@ -0,0 +1,28 @@
+// Implement the function unique_in_order 
+// which takes as argument a sequence and 
+// returns a list of items without any elements with the same value next to each other 
+// and preserving the original order of elements.
+
+function uniqueInOrder (iterable) {
+    let curr = iterable[0];
+    let result = [iterable[0]];
+    for (let i = 1; i < iterable.length; i++) {
+        if (iterable[i] !== curr) {
+            curr = iterable[i];
+            result.push(iterable[i]);
+        }
+    }
+    return result;
+}
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+describe ("uniqueInOrder", function() {
+    it("the resulting array should contain all the characters in the argument", function() {
+        expect(uniqueInOrder('ssstrringgg')).toContain('s', 't', 'r', 'i', 'n', 'g');
+    });
+
+});
+
